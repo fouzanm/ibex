771f54452ca83aec2544a0e56c0e966c31150433
Fouzan M <alfouz236@gmail.com>
2024-05-20 07:46:00 +0530

    [UPDT] ibex

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,16 +1,42 @@
 import style from './Home.module.css'
+import { motion } from "framer-motion";
+
+const textVariants = {
+    initial: {
+        x: -500,
+        opacity: 0
+    },
+    animate: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1
+        }
+    },
+    scrollButton: {
+        opacity: 0,
+        y: 10,
+        transition: {
+            duration: 2,
+            repeat: Infinity
+        }
+    }
+}
 
 const Home = () => {
     return (<div className={style.home}>
-        <div className={style.wrapper}>
+        <motion.div className={style.wrapper} variants={textVariants} initial="initial" animate="animate">
             <span className={style.subHeading}>Hello!</span>
-            <h1>I'm <span>Fouzan M</span>,</h1>
-            <h2>A developer based in Kerala, India</h2>
+            <div>
+                <h1>I'm <span>Fouzan M</span>,</h1>
+                <h2>A developer based in Kerala, India</h2>
+            </div>
             <div className={style.buttonLink}>
                 <a className={style.hire} href="#" >Hire me</a>
                 <a className={style.works} href="#" >My works</a>
             </div>
-        </div>
+        </motion.div>
         <div className={style.imageWrapper}>
             <img src='' alt='' />
         </div>
