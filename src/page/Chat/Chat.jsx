a7deab7cebd4b6668d63a77c16c8604d0fa4b184
fouzanm <alfouz236@gmail.com>
2024-06-10 13:46:26 +0530

    [UPDT] message user overflow

--- src/page/Chat/Chat.jsx
+++ src/page/Chat/Chat.jsx
@@ -27,19 +27,21 @@ const Chat = () => {
                         <input type='text' placeholder='Search'/>
                         <span><CiSearch /></span>
                     </div>
-                    <div className={style.userContainer}>
-                        <Message image={User} name='Thomas Muller' message='Hey, how are u?' time='10.25' count={1}/>
-                        <Message image={User} name='Cristiano Ronaldo' message='Hey, how are u?' time='9.58' count={0}/>
-                        <Message image={User} name='Messi' message='Hey, how are u?' time='9.45' count={3}/>
-                        <Message image={User} name='Karim Benzema' message='Hey, how are u?' time='8.50' count={1}/>
-                        <Message image={User} name='Lewandowski' message='Hey, how are u?' time='7.02' count={0}/>
-                        <Message image={User} name='Joshua Kimmich' message='Hey, how are u?' time='4.47' count={2}/>
-                        <Message image={User} name='Neuer' message='Hey, how are u?' time='4.46' count={4}/>
-                        <Message image={User} name='Jamal Musiala' message='Hey, how are u?' time='1.19' count={1}/>
-                        <Message image={User} name='Antony' message='Hey, how are u?' time='12.02' count={1}/>
-                        <Message image={User} name='Deligt' message='Hey, how are u?' time='12.02' count={1}/>
-                        <Message image={User} name='Leon Goretzka' message='Hey, how are u?' time='12.02' count={1}/>
-                        <Message image={User} name='Leroy' message='Hey, how are u?' time='12.02' count={1}/>
+                    <div className={style.userSection}>
+                        <div className={style.userContainer}>
+                            <Message image={User} name='Thomas Muller' message='Hey, how are u?' time='10.25' count={1}/>
+                            <Message image={User} name='Cristiano Ronaldo' message='Hey, how are u?' time='9.58' count={0}/>
+                            <Message image={User} name='Messi' message='Hey, how are u?' time='9.45' count={3}/>
+                            <Message image={User} name='Karim Benzema' message='Hey, how are u?' time='8.50' count={1}/>
+                            <Message image={User} name='Lewandowski' message='Hey, how are u?' time='7.02' count={0}/>
+                            <Message image={User} name='Joshua Kimmich' message='Hey, how are u?' time='4.47' count={2}/>
+                            <Message image={User} name='Neuer' message='Hey, how are u?' time='4.46' count={4}/>
+                            <Message image={User} name='Jamal Musiala' message='Hey, how are u?' time='1.19' count={1}/>
+                            <Message image={User} name='Antony' message='Hey, how are u?' time='12.02' count={1}/>
+                            <Message image={User} name='Deligt' message='Hey, how are u?' time='12.02' count={1}/>
+                            <Message image={User} name='Leon Goretzka' message='Hey, how are u?' time='12.02' count={1}/>
+                            <Message image={User} name='Leroy' message='Hey, how are u?' time='12.02' count={1}/>
+                        </div>
                     </div>
                 </div>
                 <div className={style.messageContainer}>
