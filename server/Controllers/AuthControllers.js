51cd396d222846fadd4a02b07b64feb9ef92bf00
Fouzan M <alfouz236@gmail.com>
2024-06-18 15:23:36 +0530

    [UPDT] configured server to connect logined user

---
+++ server/Controllers/AuthControllers.js
@@ -0,0 +1,52 @@
+const User = require("../Models/Users");
+const jwt = require("jsonwebtoken");
+
+const maxAge = 3*24*60*60;
+const createToken = (id) => jwt.sign({id}, "ibex login secret key", {expiresIn: maxAge});
+
+const handleErrors = (error) => {
+    let errors = {email: null, password: null};
+    if (error.message === "Incorrect Email") errors.email = "Email is not registered.";
+    else if (error.message === "Incorrect Password") errors.email = "Password is incorrect.";
+    else if (error.code === 11000) errors.email = "Email is already registered";
+    else if (err.message.includes("Users validation failed")) {
+        Object.values(err.errors).forEach(({properties}) => {
+            errors[properties.path] = properties.message
+        });
+    };
+    return errors;
+};
+
+module.exports.register = async (req, res) => {
+    try {
+        const {username, email, password} = req.body;
+        const user = await User.create({name: username, email, password});
+        const token = createToken(user._id);
+        res.cookie("ibex", token, {
+            withCredentials: true,
+            httpOnly: false,
+            maxAge: maxAge*1000,
+        });
+        res.status(201).json({user: user._id, created: true});
+    } catch (err) {
+        const errors = handleErrors(err);
+        res.json({errors, created: false});
+    };
+};
+
+module.exports.login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.cookie("ibex", token, {
+            withCredentials: true,
+            httpOnly: false,
+            maxAge: maxAge*1000,
+        });
+        res.status(200).json({user: user._id, created: true});
+    } catch (err) {
+        const errors = handleErrors(err);
+        res.json({errors, created: false});
+    };
+};
---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const routes = require("./Routes/AuthRoutes")
+
+const app = express();
+
+app.listen(4000, () => {
+    console.log("Server Started on PORT 4000")
+});
+
+mongoose.connect("mongodb://localhost:27017/ibex").then(() => {
+    console.log("MongoDB Connected...")
+}).catch((err) => {
+    console.log(err.message)
+});
+
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST"],
+    credentials: true
+}));
+
+app.use(express.json());
+app.use("/", routes);
